5fe09ea8fe1394a46873aefc88fab3459aceddee
Mrutyunjaya Sahoo <Mrutyunjaya.Sahoo@novigo.com>
2025-02-20 16:02:42 +0530

    mongodb url hardcoded

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ import os
 import certifi
 ca = certifi.where()
 
-from dotenv import load_dotenv
-load_dotenv()
-mongo_db_url = os.getenv("MONGODB_URL")
+# from dotenv import load_dotenv
+# load_dotenv()
+# mongo_db_url = os.getenv("MONGODB_URL")
+mongo_db_url = "mongodb+srv://mrutyunjayaaws1997:Admin123@cluster0.qfkfk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 print(mongo_db_url)
 import pymongo
 from networksecurity.exception.exception import NetworkSecurityException
